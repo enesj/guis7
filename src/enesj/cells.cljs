3136ce6ec1b686a426a91090fe779031f3f65ea3
enesj <enes.jakic@gmail.com>
2020-12-06 20:32:43 +0100

    Final

--- src/enesj/cells.cljs
+++ src/enesj/cells.cljs
@@ -5,7 +5,7 @@
     [com.stuartsierra.dependency :as dep]
     [clojure.string :as str]))
 
-(def cells (r/atom {}))
+(defonce cells (r/atom {}))
 
 (def columns 5)
 (def rows 10)
@@ -34,13 +34,17 @@
       (into m (mapv #(vector % k) v)))
     [] deps-map))
 
-(defn get-dependants  [id]
-  (let [g (loop [d (transform-deps (:dependencies @cells))
-                 g (dep/graph)]
-            (if (seq d)
-              (recur (next d)  (apply dep/depend g (first d)))
-              g))]
-    (dep/transitive-dependencies g id)))
+(defn get-dependants  [id dependencies]
+  (try
+    (let [g (loop [d (transform-deps (if dependencies
+                                       dependencies
+                                       (:dependencies @cells)))
+                   g (dep/graph)]
+              (if (seq d)
+                (recur (next d)  (apply dep/depend g (first d)))
+                g))]
+      (dep/transitive-dependencies g id))
+    (catch js/Object e (js/alert  (str "Error: Cyclic" (.-message e))))))
 
 (defn expand-range [cell1 cell2]
   (let [[row1 column1] (get-row-column cell1)
@@ -52,7 +56,7 @@
         (mapv ref-to-id)))))
 
 (defn sum* [vals]
-  (apply + vals))
+  (apply + (remove string? vals)))
 
 (defn prod* [vals]
   (apply * vals))
@@ -91,8 +95,9 @@
     (let [[fun-part arg-part] (is-formula expr)
           ids (get-ids arg-part)
           vals (map get-id-value ids)]
-     (swap! cells assoc-in [:dependencies kw-id] ids)
-     (fun-part (map #(apply transform-state %) vals)))
+      (when (get-dependants kw-id (assoc-in (:dependencies @cells) [kw-id] ids))
+        (swap! cells assoc-in [:dependencies kw-id] ids)
+        (fun-part (map #(apply transform-state %) vals))))
     expr))
 
 
@@ -105,23 +110,23 @@
         cell-state (kw-id (:state @cells))
         {:keys [input-state output-state]} cell-state
         show-input (= id (:selection @cells))]
-    (js/console.log id)
+
     (if show-input
-      ^{:key id} [:input {:style {:left (+ x left) :top (+ y top) :width (* width 0.8) :height (* height 0.8)   :row row :column column  :stroke-width  0.2 :position "absolute"}
-                          :id id
-                          :type "text"
-                          :value input-state
-                          :on-blur #(when
-                                      (= (is-formula ( -> % .-target .-value)) ( -> % .-target .-value))
-                                      (swap! cells assoc-in  [:state kw-id :input-state] output-state))
+      ^{:key id} [:input {:style       {:left (+ x left 6) :top (+ y top 3) :width (* width 0.8) :height (* height 0.8)  :row row :column column :stroke-width 0.2 :position "absolute"}
+                          :id          id
+                          :type        "text"
+                          :value       input-state
+                          :on-blur     #(when
+                                          (= (is-formula (-> % .-target .-value)) (-> % .-target .-value))
+                                          (swap! cells assoc-in [:state kw-id :input-state] output-state))
                           :on-key-down #(when (= (.-key %) "Enter")
                                           (swap! cells assoc :selection nil)
-                                          (input-to-output ( -> % .-target .-value) kw-id)
-                                          (let [dependants (get-dependants kw-id)]
-                                               (doseq [dependant-id dependants]
-                                                 (input-to-output (-> @cells :state dependant-id :input-state) dependant-id))))
-                          :on-change #(do
-                                        (swap! cells assoc-in  [:state kw-id :input-state] ( -> % .-target .-value)))}]
+                                          (input-to-output (-> % .-target .-value) kw-id)
+                                          (let [dependants (get-dependants kw-id nil)]
+                                            (doseq [dependant-id dependants]
+                                              (input-to-output (-> @cells :state dependant-id :input-state) dependant-id))))
+                          :on-change   #(do
+                                          (swap! cells assoc-in [:state kw-id :input-state] (-> % .-target .-value)))}]
       ^{:key id} [:div {:style {:left (+ x left) :top (+ y top) :width width :height height :border "0.2px solid gray" :position "absolute" :display "flex" :justify-content "center" :align-items "center"}
                         :on-mouse-down #(swap! cells assoc :selection id)} output-state])))
 
@@ -133,6 +138,33 @@
              (let [x (* column width)
                    y (* row height)]
                (create-cell left top x y width height row column))))))
+
+
+(defn create-row-label [left top x y width height row]
+  ^{:key row} [:div {:style       {:left (+ x left) :top (+ y top) :width width :height height :background-color "lightgray" :opacity "1"
+                                   :border "0.2px solid gray" :stroke-width 0.2 :position "absolute" :color "black" :display "flex" :justify-content "center" :align-items "center"}}
+               row])
+
+(defn create-column-label [left top x y width height column]
+  ^{:key column} [:div {:style       {:left (+ x left) :top (+ y top) :width width :height height :background-color "lightgray" :opacity "1"
+                                      :border "0.2px solid gray" :stroke-width 0.2 :position "absolute" :color "black" :display "flex" :justify-content "center" :align-items "center"}}
+                  (number-to-letter column)])
+
+(defn create-row-labels [left top full-height]
+  (let [width 30
+        height (/ full-height rows)]
+    (doall (for [row (range rows)]
+             (let [x 0
+                   y (* row height)]
+               (create-row-label left top x y width height row))))))
+
+(defn create-column-labels [left top full-width]
+  (let [width  (/ full-width columns)
+        height 40]
+    (doall (for [column (range columns)]
+             (let [x (* column width)
+                   y 0]
+               (create-column-label left top x y width height column))))))
 
 (defn init-state []
   (reset! cells
@@ -151,16 +183,18 @@
 (init-state)
 
 (defn cells-component []
-  (let [cells (r/atom {})]
-     (fn []
-       [:<>
-        (create-cells 100 150 500 500)])))
+  (fn []
+    [:<>
+     (create-row-labels 80 180 500)
+     (create-column-labels 110 140 800)
+     (create-cells 110 180 800 500)]))
 
 (defn page []
   [:div
-   [:h2 "Cells"]
+   [:h2  "Cells"]
    [cells-component]])
 
 
 (comment
+  sum, div, sub, prod
   "=sum(A1:B1)")
--- src/enesj/timer.cljs
+++ src/enesj/timer.cljs
@@ -4,20 +4,20 @@
 
 (defn timer-component []
   (let [elapsed-time (r/atom {:time 0
-                              :max 100})]
+                              :max 300})]
     (js/setInterval #(when (< (:time @elapsed-time) (:max @elapsed-time))
                        (swap! elapsed-time update :time inc))
-      1000)
+      100)
     (fn []
         [:<>
          [:label "Elapsed Time:"]
          [:meter {:min   0 :max (:max @elapsed-time)
                   :value (:time @elapsed-time)
                   :style {:width "100%"}}]
-         [:label  (:time @elapsed-time)]
+         [:label  (/ (:time @elapsed-time) 10)]
          [:input.slider {:type      "range"
                          :min       0
-                         :max 300
+                         :max 1000
                          :value     (:max @elapsed-time)
                          :on-change #(swap! elapsed-time assoc :max (-> % .-target .-value))}]
          [:button {:on-click #(swap! elapsed-time assoc :time 0)}
